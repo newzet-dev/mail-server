7fb6dedc7ae48656cd50f70c4432a1323031de5b
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-01-23 16:29:19 +0900

    feat: subscriptionJpaEntityList로 newsletterJpaEntityList 조회하는 기능 추가

--- src/main/java/com/newzet/api/user/UserRepositoryImpl.java
+++ src/main/java/com/newzet/api/user/UserRepositoryImpl.java
@@ -2,28 +2,40 @@ package com.newzet.api.user;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
+import com.newzet.api.exception.NewsletterNotFoundException;
 import com.newzet.api.exception.user.NoActiveUserException;
+import com.newzet.api.newsletter.NewsletterJpaEntity;
+import com.newzet.api.newsletter.NewsletterJpaRepository;
 import com.newzet.api.subscription.SubscriptionJpaEntity;
 import com.newzet.api.subscription.SubscriptionJpaRepository;
 
 import lombok.RequiredArgsConstructor;
 
+@Component
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
 
 	private final UserJpaRepository userJpaRepository;
 	private final SubscriptionJpaRepository subscriptionJpaRepository;
+	private final NewsletterJpaRepository newsletterJpaRepository;
 
+	// TODO: JPQL로 성능 개선, findAllBy 활용해도 됨
 	@Override
 	public ActiveUser findActiveUserByEmail(String email) {
 		UserJpaEntity userJpaEntity = userJpaRepository.findByEmailAndStatus(email,
 			UserStatus.ACTIVE).orElseThrow(NoActiveUserException::new);
 
-		// TODO: SubscriptionJpaEntity 조회
-		List<SubscriptionJpaEntity> subscriptionJpaEntity = subscriptionJpaRepository
-			.fineAllByUserId(userJpaEntity.getId());
+		List<SubscriptionJpaEntity> subscriptionJpaEntityList = subscriptionJpaRepository
+			.findAllByUserId(userJpaEntity.getId());
 
-		// TODO: NewsletterJpaEntity 조회
+		List<NewsletterJpaEntity> newsletterJpaEntityList = subscriptionJpaEntityList.stream()
+			.map(subscription -> newsletterJpaRepository.findById(subscription.getId())
+				.orElseThrow(() -> new NewsletterNotFoundException("id에 해당하는 뉴스레터가 없습니다")))
+			.toList();
+
+		// TODO User 조립
 		return null;
 	}
 }
