84549acd35848ff863f76529b44305060cec6706
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-01-23 16:24:57 +0900

    refactor: userId로 모든 SubscriptionJpaEntity 조회하는 메서드 명 오타 수정

--- src/main/java/com/newzet/api/subscription/SubscriptionJpaRepository.java
+++ src/main/java/com/newzet/api/subscription/SubscriptionJpaRepository.java
@@ -7,6 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SubscriptionJpaRepository extends JpaRepository<SubscriptionJpaEntity, Long> {
-
-	List<SubscriptionJpaEntity> fineAllByUserId(Long userId);
+	List<SubscriptionJpaEntity> findAllByUserId(Long userId);
 }
