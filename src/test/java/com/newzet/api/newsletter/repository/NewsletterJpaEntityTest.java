036965ca49461334093ae02f304c44e429ed1be9
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-01-26 14:35:54 +0900

    test: NewsletterJpaEntity를 Newsletter로 변환하는 기능 테스트 코드 추가

---
+++ src/test/java/com/newzet/api/newsletter/repository/NewsletterJpaEntityTest.java
@@ -0,0 +1,38 @@
+package com.newzet.api.newsletter.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.newzet.api.newsletter.domain.Newsletter;
+import com.newzet.api.newsletter.domain.NewsletterStatus;
+import com.newzet.api.newsletter.domain.RegisteredNewsletter;
+import com.newzet.api.newsletter.domain.UnregisteredNewsletter;
+
+class NewsletterJpaEntityTest {
+
+	@Test
+	public void 등록된_뉴스레터일때_RegisteredNewsletter_객체로_변환() {
+		//Given
+		NewsletterJpaEntity newsletterJpaEntity =
+			new NewsletterJpaEntity(1L, NewsletterStatus.REGISTERED);
+
+		//When
+		Newsletter newsletter = newsletterJpaEntity.toNewsletter();
+
+		//Then
+		Assertions.assertEquals(newsletter.getClass(), RegisteredNewsletter.class);
+	}
+
+	@Test
+	public void 등록되지_않은_뉴스레터일때_UnRegisteredNewsletter_객체로_변환() {
+		//Given
+		NewsletterJpaEntity newsletterJpaEntity =
+			new NewsletterJpaEntity(1L, NewsletterStatus.UNREGISTERED);
+
+		//When
+		Newsletter newsletter = newsletterJpaEntity.toNewsletter();
+
+		//Then
+		Assertions.assertEquals(newsletter.getClass(), UnregisteredNewsletter.class);
+	}
+}
