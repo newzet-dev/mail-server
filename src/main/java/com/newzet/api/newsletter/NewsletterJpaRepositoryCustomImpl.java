e31f622d17d5eddf8d21f12fb69d5472a6df9e72
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-01-26 13:21:53 +0900

    feat: userId로 newsletter들을 조회하는 querydsl 추가

---
+++ src/main/java/com/newzet/api/newsletter/NewsletterJpaRepositoryCustomImpl.java
@@ -0,0 +1,32 @@
+package com.newzet.api.newsletter;
+
+import static com.newzet.api.newsletter.QNewsletterJpaEntity.*;
+import static com.newzet.api.subscription.QSubscriptionJpaEntity.*;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+
+@Component
+public class NewsletterJpaRepositoryCustomImpl implements NewsletterJpaRepositoryCustom {
+
+	private final JPAQueryFactory queryFactory;
+
+	public NewsletterJpaRepositoryCustomImpl(EntityManager entityManager) {
+		this.queryFactory = new JPAQueryFactory(entityManager);
+	}
+
+	@Override
+	public List<NewsletterJpaEntity> findAllByUserId(Long userId) {
+		return queryFactory
+			.select(newsletterJpaEntity)
+			.from(subscriptionJpaEntity)
+			.join(subscriptionJpaEntity.newsletter, newsletterJpaEntity)
+			.where(subscriptionJpaEntity.user.id.eq(userId))
+			.fetch();
+	}
+}
