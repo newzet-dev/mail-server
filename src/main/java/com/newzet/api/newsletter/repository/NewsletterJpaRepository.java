73629f71902709286ea35838becba45eba964c65
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-01-27 02:19:52 +0900

    remove: userId로 newsletterList 조회하는 코드 제거

--- src/main/java/com/newzet/api/newsletter/repository/NewsletterJpaRepository.java
+++ src/main/java/com/newzet/api/newsletter/repository/NewsletterJpaRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NewsletterJpaRepository
-	extends JpaRepository<NewsletterJpaEntity, Long>, NewsletterJpaRepositoryCustom {
+public interface NewsletterJpaRepository extends JpaRepository<NewsletterJpaEntity, Long> {
 }
