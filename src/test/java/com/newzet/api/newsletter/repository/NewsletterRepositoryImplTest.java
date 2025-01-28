73f39016d1d14cef54841b3b4ff72d8e23234c05
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-01-28 21:13:38 +0900

    test: 뉴스레터 저장하는 테스트 코드 추가

--- src/test/java/com/newzet/api/newsletter/repository/NewsletterRepositoryImplTest.java
+++ src/test/java/com/newzet/api/newsletter/repository/NewsletterRepositoryImplTest.java
@@ -1,75 +1,36 @@
 package com.newzet.api.newsletter.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
-import java.util.Optional;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import com.newzet.api.newsletter.domain.Newsletter;
-import com.newzet.api.newsletter.domain.RegisteredNewsletter;
+import com.newzet.api.newsletter.domain.NewsletterStatus;
 
+@SpringBootTest
 class NewsletterRepositoryImplTest {
 
-	private NewsletterJpaRepository newsletterJpaRepository;
+	@Autowired
 	private NewsletterRepositoryImpl newsletterRepository;
 
-	@BeforeEach
-	void setUp() {
-		newsletterJpaRepository = Mockito.mock(NewsletterJpaRepository.class);
-		newsletterRepository = new NewsletterRepositoryImpl(newsletterJpaRepository);
-	}
-
 	@Test
-	public void UnregisteredNewsletter를_생성_및_반환() {
-		//Given
+	public void 뉴스레터_저장() {
+		// Given
 		String name = "test";
 		String domain = "test@example.com";
-		String maillingList = "test1234";
+		String maillingList = "test123";
+		NewsletterStatus status = NewsletterStatus.REGISTERED;
 
-		newsletterRepository.createAndGetUnregisteredNewsletter(name, domain, maillingList);
+		// When
+		Newsletter savedNewsletter = newsletterRepository.save(name, domain, maillingList, status);
 
-		//When
-
-		//Then
-	}
-
-	@Test
-	public void 뉴스레터가_존재하는_경우_뉴스레터를_리턴() {
-		//Given
-		String existDomain = "exist@example.com";
-		String existMaillingList = "exist1234";
-		NewsletterEntity newsletterEntity = Mockito.mock(NewsletterEntity.class);
-		when(newsletterEntity.toNewsletter()).thenReturn(new RegisteredNewsletter());
-		when(newsletterJpaRepository
-			.findNewsletterByDomainOrMaillingList(existDomain, existMaillingList))
-			.thenReturn(Optional.of(newsletterEntity));
-
-		//When
-		Newsletter newsletter = newsletterRepository.findNewsletter(existDomain, existMaillingList);
-
-		//Then
-		assertInstanceOf(Newsletter.class, newsletter);
-		verify(newsletterEntity).toNewsletter();
-	}
-
-	@Test
-	public void 뉴스레터가_존재하지_않는_경우_null을_리턴() {
-		//Given
-		String noExistDomain = "no_exist@example.com";
-		String noExistMaillingList = "no_exist1234";
-		when(newsletterJpaRepository
-			.findNewsletterByDomainOrMaillingList(noExistDomain, noExistMaillingList))
-			.thenReturn(Optional.empty());
-
-		//When
-		Newsletter newsletter = newsletterRepository
-			.findNewsletter(noExistDomain, noExistMaillingList);
-
-		//Then
-		assertNull(newsletter);
+		// Then
+		assertEquals(1L, savedNewsletter.getId());
+		assertEquals(name, savedNewsletter.getName());
+		assertEquals(domain, savedNewsletter.getDomain());
+		assertEquals(maillingList, savedNewsletter.getMaillingList());
+		assertEquals(status, savedNewsletter.getStatus());
 	}
 }
