d72a77c81ad1670eda6057d749c11d3530a7e772
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-01-31 16:31:59 +0900

    test: findByDomain 메서드 호출 시 값이 존재하면 newsletterEntity를 반환하는 테스트 코드 추가

---
+++ src/test/java/com/newzet/api/newsletter/repository/NewsletterRedisRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.newzet.api.newsletter.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@DataRedisTest
+class NewsletterRedisRepositoryTest {
+
+	@Autowired
+	private RedisTemplate<String, String> redisTemplate;
+
+	private NewsletterRedisRepository newsletterRedisRepository;
+
+	@BeforeEach
+	void setUp() {
+		newsletterRedisRepository = new NewsletterRedisRepository(redisTemplate,
+			new ObjectMapper());
+	}
+
+	@AfterEach
+	void teardown() {
+		newsletterRedisRepository.deleteAll();
+	}
+
+	@Test
+	public void findByDomain_존재하면_NewsletterEntity를_반환() {
+		//Given
+		String domain = "exist@example.com";
+		NewsletterEntity newsletter = NewsletterEntity.builder()
+			.domain(domain)
+			.build();
+		newsletterRedisRepository.saveByDomain(domain, newsletter);
+
+		//When
+		Optional<NewsletterEntity> foundNewsletter = newsletterRedisRepository.findByDomain(domain);
+
+		//Then
+		assertTrue(foundNewsletter.isPresent());
+		assertEquals(domain, foundNewsletter.get().getDomain());
+	}
+
+}
