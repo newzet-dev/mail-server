5618d54296947b2bae04033bf75b1e45e987a7bd
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-02-24 23:10:54 +0900

    feat: 역직렬화 실패 시 예외 클래스 정의

---
+++ src/main/java/com/newzet/api/common/objectMapper/DeserializationException.java
@@ -0,0 +1,7 @@
+package com.newzet.api.common.objectMapper;
+
+public class DeserializationException extends RuntimeException {
+	public DeserializationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
--- src/main/java/com/newzet/api/newsletter/exception/RedisSerializationException.java
+++
@@ -1,7 +0,0 @@
-package com.newzet.api.newsletter.exception;
-
-public class RedisSerializationException extends RuntimeException {
-	public RedisSerializationException(String message, Throwable cause) {
-		super(message, cause);
-	}
-}
