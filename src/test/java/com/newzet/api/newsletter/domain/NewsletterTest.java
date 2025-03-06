66b351fb336f74deab9f65a2de085125313250cf
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-03-06 11:56:39 +0900

    test: newsletter를 cacheDto로 변환

--- src/test/java/com/newzet/api/newsletter/domain/NewsletterTest.java
+++ src/test/java/com/newzet/api/newsletter/domain/NewsletterTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import com.newzet.api.newsletter.business.NewsletterCacheDto;
+
 class NewsletterTest {
 
 	@Test
@@ -18,5 +20,26 @@ class NewsletterTest {
 		assertEquals("test@example.com", newsletter.getDomain());
 		assertEquals("test123", newsletter.getMailingList());
 		assertEquals(NewsletterStatus.UNREGISTERED, newsletter.getStatus());
+	}
+
+	@Test
+	public void toCacheDto_returnNewsletterCacheDto() {
+		//Given
+		Long id = 1L;
+		String name = "test";
+		String domain = "test@example.com";
+		String mailingList = "test123";
+		String status = "UNREGISTERED";
+		Newsletter newsletter = Newsletter.create(id, name, domain, mailingList, status);
+
+		//When
+		NewsletterCacheDto dto = newsletter.toCacheDto();
+
+		//Then
+		assertEquals(newsletter.getId(), dto.getId());
+		assertEquals(newsletter.getName(), dto.getName());
+		assertEquals(newsletter.getDomain(), dto.getDomain());
+		assertEquals(newsletter.getMailingList(), dto.getMailingList());
+		assertEquals(newsletter.getStatus().name(), dto.getStatus());
 	}
 }
