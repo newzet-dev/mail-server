f232b459a41f318adf04e5cdf686b20df2b8ef93
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-03-06 11:57:11 +0900

    feat: newsletter를 NewsletterCacheDto로 변환하는 기능 추가

--- src/main/java/com/newzet/api/newsletter/domain/Newsletter.java
+++ src/main/java/com/newzet/api/newsletter/domain/Newsletter.java
@@ -1,5 +1,7 @@
 package com.newzet.api.newsletter.domain;
 
+import com.newzet.api.newsletter.business.NewsletterCacheDto;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,7 +16,11 @@ public class Newsletter {
 	private final NewsletterStatus status;
 
 	public static Newsletter create(Long id, String name, String domain, String mailingList,
-		NewsletterStatus status) {
-		return new Newsletter(id, name, domain, mailingList, status);
+		String status) {
+		return new Newsletter(id, name, domain, mailingList, NewsletterStatus.valueOf(status));
+	}
+
+	public NewsletterCacheDto toCacheDto() {
+		return NewsletterCacheDto.create(id, name, domain, mailingList, status.name());
 	}
 }
