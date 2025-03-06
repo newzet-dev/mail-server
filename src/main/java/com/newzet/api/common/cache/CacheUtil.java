ea505e035909a6d9cbee57e5f35ff507f7365c80
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-03-06 11:21:52 +0900

    feat: cacheUtil 인터페이스 및 구현체 등록

---
+++ src/main/java/com/newzet/api/common/cache/CacheUtil.java
@@ -0,0 +1,9 @@
+package com.newzet.api.common.cache;
+
+import java.util.Optional;
+
+public interface CacheUtil {
+	public <T> Optional<T> get(String key, Class<T> classType);
+
+	public Boolean set(String key, Object object, long ttl);
+}
---
+++ src/main/java/com/newzet/api/common/cache/RedisUtil.java
@@ -0,0 +1,30 @@
+package com.newzet.api.common.cache;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import com.newzet.api.common.objectMapper.OptionalObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class RedisUtil implements CacheUtil {
+	private static final TimeUnit TIME_UNIT = TimeUnit.MILLISECONDS;
+
+	private final RedisTemplate<String, String> redisTemplate;
+	private final OptionalObjectMapper objectMapper;
+
+	@Override
+	public <T> Optional<T> get(String key, Class<T> classType) {
+		return Optional.empty();
+	}
+
+	@Override
+	public Boolean set(String key, Object object, long ttl) {
+		return true;
+	}
+}
