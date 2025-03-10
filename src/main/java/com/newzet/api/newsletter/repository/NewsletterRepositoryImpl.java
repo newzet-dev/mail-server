3ad029aeba2a875cf49335db8d3b3b113614347f
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-03-10 10:49:40 +0900

    feat: domain과 mailingList로 newsletterEntity를 조회하는 기능 추가

--- src/main/java/com/newzet/api/newsletter/repository/NewsletterRepositoryImpl.java
+++ src/main/java/com/newzet/api/newsletter/repository/NewsletterRepositoryImpl.java
@@ -3,7 +3,6 @@ package com.newzet.api.newsletter.repository;
 import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.newzet.api.newsletter.business.NewsletterRepository;
 import com.newzet.api.newsletter.business.dto.NewsletterEntityDto;
@@ -17,7 +16,6 @@ public class NewsletterRepositoryImpl implements NewsletterRepository {
 	private final NewsletterJpaRepository newsletterJpaRepository;
 
 	@Override
-	@Transactional
 	public NewsletterEntityDto save(String name, String domain, String mailingList, String status) {
 		NewsletterEntity newsletterEntity = NewsletterEntity.create(name, domain, mailingList,
 			status);
@@ -28,8 +26,7 @@ public class NewsletterRepositoryImpl implements NewsletterRepository {
 	@Override
 	public Optional<NewsletterEntityDto> findByDomainOrMailingList(String domain,
 		String mailingList) {
-		return Optional.empty();
-		// return newsletterJpaRepository.findNewsletterByDomainOrMailingList(domain, mailingList)
-		// 	.map(NewsletterEntity::toModel);
+		return newsletterJpaRepository.findNewsletterByDomainOrMailingList(domain, mailingList)
+			.map(NewsletterEntity::toEntityDto);
 	}
 }
