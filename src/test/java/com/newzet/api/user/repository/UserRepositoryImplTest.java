a38dcf54c911f2a427a69dcdf105ab797ba1d195
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-03-11 21:25:46 +0900

    test: user 저장 기능

---
+++ src/test/java/com/newzet/api/user/repository/UserRepositoryImplTest.java
@@ -0,0 +1,37 @@
+package com.newzet.api.user.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import com.newzet.api.config.PostgresTestContainerConfig;
+import com.newzet.api.user.business.dto.UserEntityDto;
+
+@DataJpaTest
+@Import(UserRepositoryImpl.class)
+@ExtendWith(PostgresTestContainerConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserRepositoryImplTest {
+
+	@Autowired
+	private UserRepositoryImpl userRepository;
+
+	@Test
+	public void save() {
+		//Given
+		String email = "exist@example.com";
+		String status = "ACTIVE";
+
+		//When
+		UserEntityDto savedUser = userRepository.save(email, status);
+
+		//Then
+		assertEquals(email, savedUser.getEmail());
+		assertEquals(status, savedUser.getStatus());
+	}
+}
