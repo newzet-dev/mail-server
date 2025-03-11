7323bf448ec9062d6f5881001e954f4a55f5738d
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-03-11 21:23:28 +0900

    feat: UserRepository 인터페이스 정의

---
+++ src/main/java/com/newzet/api/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.newzet.api.user.repository;
+
+import com.newzet.api.user.business.dto.UserEntityDto;
+
+public interface UserRepository {
+	UserEntityDto save(String email, String status);
+
+	UserEntityDto getByEmail(String email);
+}
