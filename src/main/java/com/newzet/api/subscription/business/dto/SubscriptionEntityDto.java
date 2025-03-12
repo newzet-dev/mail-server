559c974a70e8e260d1384af520b1b3962b4c5a45
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-03-12 15:30:17 +0900

    feat: subscriptionEntityDto 정의

---
+++ src/main/java/com/newzet/api/subscription/business/dto/SubscriptionEntityDto.java
@@ -0,0 +1,20 @@
+package com.newzet.api.subscription.business.dto;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.newzet.api.newsletter.business.dto.NewsletterEntityDto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Builder(access = AccessLevel.PROTECTED)
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class SubscriptionEntityDto {
+	private final UUID id;
+	private final NewsletterEntityDto newsletterEntityDto;
+	private final LocalDateTime createdAt;
+}
