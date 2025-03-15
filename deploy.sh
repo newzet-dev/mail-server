98051d4aecc97be466e633a3e7337be9c05fa48c
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-03-15 11:20:59 +0900

    feat: deploy.sh에서 env 파일과 함꼐 컨테이너를 실행하도록 설정

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ docker-compose -f docker-compose.yml down || true
 aws s3 cp s3://newzet-config/.env .env
 echo "IMAGE_TAG=$1" >> .env
 
-docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose.yml --env-file .env up -d
 
 timeout 60s bash -c "until curl -s http://localhost:8080/actuator/health | grep -q '\"status\":\"UP\"'; do sleep 5; echo 'Waiting for health check...'; done"
 if [ $? -ne 0 ]; then
