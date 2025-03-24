d4090ec0a315eb4959df86618b244161f3666845
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-03-24 21:32:58 +0900

    feat: RedisLock 획득 실패 예외 추가

---
+++ src/main/java/com/newzet/api/common/cache/exception/RedisLockAcquisitionException.java
@@ -0,0 +1,9 @@
+package com.newzet.api.common.cache.exception;
+
+import lombok.extern.slf4j.Slf4j;
+
+public class RedisLockAcquisitionException extends RuntimeException {
+	public RedisLockAcquisitionException() {
+		super();
+	}
+}
