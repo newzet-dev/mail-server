030e4b635813eaf7a74924b92a68b6b8374d9d9d
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-03-25 21:24:53 +0900

    test: 이미 락 획득 시 Lock 획득에 실패하는 테스트

--- src/test/java/com/newzet/api/common/lock/LocalLockFactoryTest.java
+++ src/test/java/com/newzet/api/common/lock/LocalLockFactoryTest.java
@@ -3,6 +3,7 @@ package com.newzet.api.common.lock;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.Lock;
 
 import org.junit.jupiter.api.Assertions;
@@ -25,5 +26,30 @@ public class LocalLockFactoryTest {
 	    //Then
 		assertTrue(lock.isPresent());
 		lock.get().unlock();
+	}
+	
+	@Test
+	public void tryLock_whenLockAlreadyHeld_returnEmpty() throws InterruptedException{
+		// Given
+		String lockKey = "testLock";
+
+		// When
+		Optional<Lock> firstLock = localLockFactory.tryLock(lockKey, 500, 5000);
+		assertTrue(firstLock.isPresent());
+
+		// 다른 스레드에서 락 시도 → 실패해야 함
+		AtomicBoolean secondAcquired = new AtomicBoolean(true);
+		Thread thread = new Thread(() -> {
+			Optional<Lock> secondLock = localLockFactory.tryLock(lockKey, 500, 5000);
+			secondAcquired.set(secondLock.isPresent());
+		});
+		thread.start();
+		thread.join();
+
+		// Then
+		assertFalse(secondAcquired.get());
+
+		// Cleanup
+		firstLock.get().unlock();
 	}
 }
