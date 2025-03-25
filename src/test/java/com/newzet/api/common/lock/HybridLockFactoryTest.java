6abb107807d2f83e90982284a8668fba6afd427a
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-03-25 21:37:24 +0900

    test: redisLockFactory가 redisLockAcquisition 예외를 던질 때 LocalLock을 획득하는 테스트

--- src/test/java/com/newzet/api/common/lock/HybridLockFactoryTest.java
+++ src/test/java/com/newzet/api/common/lock/HybridLockFactoryTest.java
@@ -12,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.newzet.api.common.lock.exception.RedisLockAcquisitionException;
 import com.newzet.api.common.lock.local.LocalLock;
 import com.newzet.api.common.lock.local.LocalLockFactory;
 import com.newzet.api.common.lock.redis.RedisLock;
@@ -60,10 +61,27 @@ class HybridLockFactoryTest {
 			.thenReturn(Optional.of(localLock));
 
 		// When
-		Optional<Lock> result = hybridLockFactory.tryLock(key, 500, 1000);
+		Optional<Lock> lock = hybridLockFactory.tryLock(key, 500, 1000);
 
 		// Then
-		assertTrue(result.isPresent());
-		assertEquals(localLock, result.get());
+		assertTrue(lock.isPresent());
+		assertEquals(localLock, lock.get());
+	}
+	
+	@Test
+	public void tryLock_whenThrowRedisLockAcquisitionException_returnLocalLock() {
+		// Given
+		String key = "testLock";
+		when(redisLockFactory.tryLock(eq(key), anyLong(), anyLong()))
+			.thenThrow(new RedisLockAcquisitionException());
+		when(localLockFactory.tryLock(eq(key), anyLong(), anyLong()))
+			.thenReturn(Optional.of(localLock));
+
+		// When
+		Optional<Lock> lock = hybridLockFactory.tryLock(key, 500, 1000);
+
+		// Then
+		assertTrue(lock.isPresent());
+		assertEquals(localLock, lock.get());
 	}
 }
