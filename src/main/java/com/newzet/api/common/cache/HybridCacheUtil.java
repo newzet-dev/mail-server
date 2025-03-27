160730d66bfa3663532dc27cf00b74154b332016
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-03-27 15:42:05 +0900

    feat: redisUtil 조회 후 장애 시 localCacheUtil을 사용하는 기능 추가

---
+++ src/main/java/com/newzet/api/common/cache/HybridCacheUtil.java
@@ -0,0 +1,42 @@
+package com.newzet.api.common.cache;
+
+import java.util.Optional;
+
+import javax.cache.CacheException;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import com.newzet.api.common.cache.local.LocalCacheUtil;
+import com.newzet.api.common.cache.redis.RedisServerException;
+import com.newzet.api.common.cache.redis.RedisUtil;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Primary
+@Component
+@RequiredArgsConstructor
+public class HybridCacheUtil implements CacheUtil{
+	private final RedisUtil redisUtil;
+	private final LocalCacheUtil localCacheUtil;
+
+	@Override
+	public <T> Optional<T> get(String key, Class<T> classType) {
+		try{
+			return redisUtil.get(key, classType);
+		} catch(RedisServerException e) {
+			return localCacheUtil.get(key, classType);
+		}
+	}
+
+	@Override
+	public <T> Boolean set(String key, T object, long ttl) {
+		try{
+			return redisUtil.set(key, object, ttl);
+		} catch(CacheException e) {
+			return localCacheUtil.set(key, object, ttl);
+		}
+	}
+}
