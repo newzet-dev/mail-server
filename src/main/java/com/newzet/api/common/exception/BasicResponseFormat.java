b985804dd9f91585ccc2f8162026e3295dc6ce29
anacnoda77 <sm1108shin@naver.com>
2025-04-08 16:34:27 +0900

    Feat: 클래스명 변경 및 Exception에 ResponseCode 멤버 변수 추가

--- src/main/java/com/newzet/api/common/exception/BasicResponseFormat.java
+++ src/main/java/com/newzet/api/common/exception/BasicResponseFormat.java
@@ -1,18 +1,15 @@
-package com.newzet.api.newsletter.exception;
-
-import com.newzet.api.common.exception.ResponseCode;
-import com.newzet.api.common.exception.ResponseFormat;
+package com.newzet.api.common.exception;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class NewsletterResponseFormat implements ResponseFormat {
+public class BasicResponseFormat implements ResponseFormat {
 
 	private final ResponseCode responseCode;
 	private final String message;
 
 	public ResponseFormat createResponse(ResponseCode responseCode, String message) {
-		return new NewsletterResponseFormat(responseCode, message);
+		return new BasicResponseFormat(responseCode, message);
 	}
 
 	@Override
--- src/main/java/com/newzet/api/common/exception/NewzetException.java
+++ src/main/java/com/newzet/api/common/exception/NewzetException.java
@@ -2,12 +2,18 @@ package com.newzet.api.common.exception;
 
 public abstract class NewzetException extends RuntimeException {
 	private final String message;
+	private final ResponseCode responseCode;
 
-	protected NewzetException(String message) {
+	protected NewzetException(String message, ResponseCode responseCode) {
 		this.message = message;
+		this.responseCode = responseCode;
 	}
 
 	public String getMessage() {
 		return message;
+	}
+
+	public ResponseCode getResponseCode() {
+		return responseCode;
 	}
 }
