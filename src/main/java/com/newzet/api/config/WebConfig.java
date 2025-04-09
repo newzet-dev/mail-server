a5c8102ab5b156f3bd1af019ea46194259be8f6a
gitjiho <jiho99322@gmail.com>
2025-04-09 23:28:27 +0900

    Feat: 인터셉터, 리졸버 WebMVC 오버라이딩 및 필터 필터 Bean 등록

--- src/main/java/com/newzet/api/config/WebConfig.java
+++ src/main/java/com/newzet/api/config/WebConfig.java
@@ -1,14 +1,52 @@
 package com.newzet.api.config;
 
+import java.util.List;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.newzet.api.auth.infrastructure.filter.AuthExceptionFilter;
+import com.newzet.api.auth.infrastructure.interceptor.JwtAuthInterceptor;
+import com.newzet.api.auth.infrastructure.resolver.AuthUserArgumentResolver;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+	private final JwtAuthInterceptor jwtAuthInterceptor;
+	private final AuthUserArgumentResolver authUserArgumentResolver;
+	private final AuthExceptionFilter authExceptionFilter;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(jwtAuthInterceptor)
+			.order(1)
+			.addPathPatterns("/**");
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(authUserArgumentResolver);
+	}
+
+	@Bean
+	public FilterRegistrationBean<AuthExceptionFilter> authExceptionFilterRegistration() {
+		FilterRegistrationBean<AuthExceptionFilter> registration = new FilterRegistrationBean<>();
+		registration.setFilter(authExceptionFilter);
+		registration.addUrlPatterns("/*");
+		registration.setName("authExceptionFilter");
+		registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return registration;
+	}
 
 	@Bean
 	public CorsFilter corsFilter() {
