0a5e2aab847c52918a91ffd86c0ae1a8a8c2e59d
gitjiho <jiho99322@gmail.com>
2025-04-09 23:20:27 +0900

    Feat: TokenRepository 인터페이스 생성

---
+++ src/main/java/com/newzet/api/auth/infrastructure/TokenRepository.java
@@ -0,0 +1,17 @@
+package com.newzet.api.auth.infrastructure;
+
+import java.util.Optional;
+
+import com.newzet.api.auth.domain.Token;
+
+public interface TokenRepository {
+	void saveToken(String userId, String deviceType, Token token);
+
+	Optional<Token> findToken(String userId, String deviceType);
+
+	void removeToken(String userId, String deviceType);
+
+	Long getLastRefreshTime(String userId, String deviceType);
+
+	void updateLastRefreshTime(String userId, String deviceType);
+}
