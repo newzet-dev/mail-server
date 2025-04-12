09db964facc8bb9824c7498872fa105fa430a01c
anacnoda77 <sm1108shin@naver.com>
2025-04-12 20:08:23 +0900

    Feat: InternalErrorException 상태코드 50000으로 변경

--- src/main/java/com/newzet/api/common/exception/InternalErrorException.java
+++ src/main/java/com/newzet/api/common/exception/InternalErrorException.java
@@ -3,7 +3,7 @@ package com.newzet.api.common.exception;
 import com.newzet.api.common.response.ResponseCode;
 
 public class InternalErrorException extends NewzetException {
-	public static final ResponseCode responseCode = ResponseCode.CONFLICT;
+	public static final ResponseCode responseCode = ResponseCode.SERVER_ERROR;
 
 	public InternalErrorException(String message) {
 		super(message, responseCode);
