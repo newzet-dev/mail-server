89bfe60a845cb03e0fb4bebeb2f14ac8bc4da6e9
anacnoda77 <sm1108shin@naver.com>
2025-04-14 17:06:42 +0900

    Feat: 핸들러에 InternalErrorException, Exception 핸들링 메서드 추가

--- src/main/java/com/newzet/api/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/newzet/api/common/exception/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.newzet.api.common.response.ResponseCode;
+
 import lombok.extern.slf4j.Slf4j;
 
 @RestControllerAdvice
@@ -14,9 +16,27 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(NewzetException.class)
 	public ProblemDetail handleNewzetEx(NewzetException e) {
 		ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.OK);
-		problemDetail.setTitle("Newzet Error");
+		problemDetail.setTitle("Newzet Service Error");
 		problemDetail.setProperty("code", e.getResponseCode().getCode());
 		problemDetail.setProperty("message", e.getMessage());
+		return problemDetail;
+	}
+
+	@ExceptionHandler(InternalErrorException.class)
+	public ProblemDetail handleInternalErrorEx(InternalErrorException e) {
+		ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.OK);
+		problemDetail.setTitle("Internal Error");
+		problemDetail.setProperty("code", "내부에서 요청 처리에 실패하였습니다. 다시 시도해주세요.");
+		problemDetail.setProperty("message", e.getMessage());
+		return problemDetail;
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ProblemDetail handleUnknownException(Exception e) {
+		ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.OK);
+		problemDetail.setTitle("Unknown Error");
+		problemDetail.setProperty("code", ResponseCode.SERVER_ERROR);
+		problemDetail.setProperty("message", "알 수 없는 서버 내부 오류가 발생하였습니다.");
 		return problemDetail;
 	}
 }
