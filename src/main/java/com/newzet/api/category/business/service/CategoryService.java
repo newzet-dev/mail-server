d2318767de3218a89057e302be03a61da8d959cf
anacnoda77 <sm1108shin@naver.com>
2025-04-17 17:03:25 +0900

    Feat: 읽기 메서드에 readOnly 속성 적용

--- src/main/java/com/newzet/api/category/business/service/CategoryService.java
+++ src/main/java/com/newzet/api/category/business/service/CategoryService.java
@@ -13,7 +13,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class CategoryService {
 
 	private final CategoryRepository categoryRepository;
