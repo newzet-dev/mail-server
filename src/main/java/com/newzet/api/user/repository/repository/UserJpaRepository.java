8b22da84803caa50479d881e1150190a1a847d06
gitjiho <jiho99322@gmail.com>
2025-04-19 11:23:07 +0900

    Fix: UserJpaRepository pk Long -> UUID 변경

--- src/main/java/com/newzet/api/user/repository/repository/UserJpaRepository.java
+++ src/main/java/com/newzet/api/user/repository/repository/UserJpaRepository.java
@@ -1,6 +1,7 @@
 package com.newzet.api.user.repository.repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,6 @@ import org.springframework.stereotype.Repository;
 import com.newzet.api.user.repository.entity.UserEntity;
 
 @Repository
-public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
+public interface UserJpaRepository extends JpaRepository<UserEntity, UUID> {
 	Optional<UserEntity> findByEmail(String email);
 }
