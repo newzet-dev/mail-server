39d263a086a7f2421ee07f706beced3a316f929e
gitjiho <jiho99322@gmail.com>
2025-04-25 13:38:55 +0900

    Feat: UserController에 추가된 비즈니스 로직 기반 수정 및 리턴타입에 SuccessResponse 도입

--- src/main/java/com/newzet/api/user/controller/UserController.java
+++ src/main/java/com/newzet/api/user/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.newzet.api.user.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.newzet.api.auth.business.dto.JwtResponse;
+import com.newzet.api.common.response.ResponseCode;
+import com.newzet.api.common.response.SuccessResponse;
 import com.newzet.api.user.business.dto.SignupRequest;
 import com.newzet.api.user.business.dto.UniqueMailResponse;
 import com.newzet.api.user.business.service.UserService;
@@ -30,19 +31,28 @@ public class UserController {
 	@PostMapping("/signup")
 	@Operation(summary = "회원 가입",
 		description = "Oauth 로직 통과 후 매칭되는 유저가 없을 시 회원가입을 진행한다.")
-	public ResponseEntity<JwtResponse> signup(@Valid @RequestBody SignupRequest request) {
+	public ResponseEntity<SuccessResponse<JwtResponse>> signup(
+		@Valid @RequestBody SignupRequest request) {
 		JwtResponse jwtResponse = userService.signUp(request);
-		return ResponseEntity.status(HttpStatus.CREATED).body(jwtResponse);
+		SuccessResponse<JwtResponse> response = SuccessResponse.create(
+			ResponseCode.SUCCESS, "회원가입 성공", jwtResponse);
+		return ResponseEntity.ok(response);
 	}
 
 	@GetMapping("/my/mail")
 	@Operation(summary = "메일 중복조회",
 		description = "메일이 사용 가능한지 조회한다. 휴면유저/탈퇴한 유저의 메일도 사용 불가.")
-	public ResponseEntity<UniqueMailResponse> checkEmailUniqueness(
+	public ResponseEntity<SuccessResponse<UniqueMailResponse>> checkEmailUniqueness(
 		@RequestParam("v") String email) {
 		UniqueMailResponse result = userService.checkEmailUniqueness(email);
-		return ResponseEntity.ok(result);
+		SuccessResponse<UniqueMailResponse> response = SuccessResponse.create(
+			ResponseCode.SUCCESS, "메일 중복조회 성공", result
+		);
+		return ResponseEntity.ok(response);
 	}
 
 	//TODO: 로그아웃
+	//TODO: 회원 탈퇴
+	//TODO: user 정보 수정
+	//TODO: 유저 정보 등록 여부 확인
 }
