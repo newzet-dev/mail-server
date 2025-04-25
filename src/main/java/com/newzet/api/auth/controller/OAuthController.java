549517a8b79d3d6f5a12d5b2d882a2ed0a30f488
gitjiho <jiho99322@gmail.com>
2025-04-25 13:37:11 +0900

    Feat: Oauth Controller 구현

---
+++ src/main/java/com/newzet/api/auth/controller/OAuthController.java
@@ -0,0 +1,58 @@
+package com.newzet.api.auth.controller;
+
+import java.net.URI;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.newzet.api.auth.business.service.OAuthLoginService;
+import com.newzet.api.auth.domain.OAuthProvider;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth/oauth")
+@Tag(name = "OAuth", description = "OAuth 관련 API")
+public class OAuthController {
+
+	private final OAuthLoginService oAuthLoginService;
+
+	@GetMapping("/{provider}/login")
+	@Operation(summary = "OAuth 로그인 페이지 이동",
+		description = "provider별 로그인 페이지로 이동한다. state는 deviceType")
+	public RedirectView redirectToOAuthLoginPage(
+		@PathVariable("provider") String provider,
+		@RequestParam(value = "state", required = false) String state) {
+
+		OAuthProvider oAuthProvider = OAuthProvider.valueOf(provider.toUpperCase());
+		String redirectUrl = oAuthLoginService.getOAuthLoginUrl(oAuthProvider, state);
+		return new RedirectView(redirectUrl);
+	}
+
+	@GetMapping("/{provider}/callback")
+	@Operation(summary = "OAuth용 콜백 [직접 사용 금지]",
+		description = "provider 측에서 콜백 리다이렉트로 사용할 엔드포인트. 직접 사용 금지.")
+	public ResponseEntity<Void> handleOAuthCallback(
+		@PathVariable("provider") String provider,
+		@RequestParam("code") String code,
+		@RequestParam(value = "state", required = false) String state) {
+
+		OAuthProvider oAuthProvider = OAuthProvider.valueOf(provider.toUpperCase());
+		URI redirectInfo = oAuthLoginService.handleOAuthCallback(oAuthProvider, code,
+			state);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(redirectInfo);
+		return new ResponseEntity<>(headers, HttpStatus.FOUND);
+	}
+}
