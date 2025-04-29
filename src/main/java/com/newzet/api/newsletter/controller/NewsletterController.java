0bc3512a78733d00020a360c286d8f23d35ea874
anacnoda77 <sm1108shin@naver.com>
2025-04-29 17:17:42 +0900

    Feat: 뉴스레터 추천 API 구현 및 스웨거 적용

--- src/main/java/com/newzet/api/newsletter/controller/NewsletterController.java
+++ src/main/java/com/newzet/api/newsletter/controller/NewsletterController.java
@@ -7,11 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.newzet.api.auth.domain.AuthUser;
+import com.newzet.api.auth.infrastructure.annotation.Login;
+import com.newzet.api.auth.infrastructure.annotation.RequireAuth;
 import com.newzet.api.common.response.ResponseCode;
 import com.newzet.api.common.response.SuccessResponse;
 import com.newzet.api.newsletter.business.NewsletterService;
 import com.newzet.api.newsletter.controller.dto.NewsletterInfoResponse;
 import com.newzet.api.newsletter.controller.dto.NewsletterListResponse;
+import com.newzet.api.newsletter.controller.dto.NewsletterRecommendResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -49,6 +53,19 @@ public class NewsletterController {
 		SuccessResponse<NewsletterInfoResponse> response = SuccessResponse.create(
 			ResponseCode.SUCCESS, "뉴스레터 상세정보 조회 성공", newsletterInfoResponse);
 
+		return ResponseEntity.ok(response);
+	}
+
+	@GetMapping("/recommend")
+	@RequireAuth
+	@Operation(summary = "뉴스레터 추천 리스트 조회",
+		description = "유저 id로 뉴스레터 추천 리스트를 조회한다.")
+	public ResponseEntity<SuccessResponse<NewsletterRecommendResponse>> recommendNewsletterList(
+		@Login AuthUser authUser) {
+		NewsletterRecommendResponse newsletterRecommendResponse = newsletterService.recommendNewsletterList(
+			authUser.getId());
+		SuccessResponse<NewsletterRecommendResponse> response = SuccessResponse.create(
+			ResponseCode.SUCCESS, "뉴스레터 추천 성공", newsletterRecommendResponse);
 		return ResponseEntity.ok(response);
 	}
 }
