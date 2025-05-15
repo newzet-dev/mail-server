d0db7493ab4a394674195618209356987b9b1f3b
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-05-15 20:19:43 +0900

    feat: 메일 수신 시 구독 처리 기능 추가

--- src/main/java/com/newzet/api/mail/business/MailServiceFacade.java
+++ src/main/java/com/newzet/api/mail/business/MailServiceFacade.java
@@ -1,13 +1,13 @@
 package com.newzet.api.mail.business;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.newzet.api.newsletter.business.service.NewsletterService;
-import com.newzet.api.newsletter.domain.model.Newsletter;
 import com.newzet.api.subscription.business.service.SubscriptionService;
 import com.newzet.api.user.business.service.UserService;
-import com.newzet.api.user.domain.User;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,9 +22,10 @@ public class MailServiceFacade {
 
 	public void processMail(String fromName, String fromDomain, String toDomain, String mailingList,
 		String htmlLink) {
-		User user = userService.getUserByEmail(toDomain);
-		Newsletter newsletter = newsletterService.findOrCreateNewsletter(fromName,
-			fromDomain, mailingList);
-		subscriptionService.addSubscription(user, newsletter);
+		UUID userId = userService.getUserIdByEmail(toDomain);
+		subscriptionService.addSubscriptionIfUnsubscribed(userId, fromName, fromDomain, mailingList);
+
+		//TODO: Article 저장 구현 (배치처리 + 비동기)
+		//TODO: 아티클 저장 후 배치처리 된 애들에 대해서 별도로 event 형식으로 fcm noti 보내는거 추가 (여기 말고)
 	}
 }
