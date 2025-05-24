a3949f68e24a5a72674e3de2fd8fe5e7c065d882
anacnoda77 <sm1108shin@naver.com>
2025-05-24 18:24:37 +0900

    Feat: 구독 삭제 API 구현

--- src/main/java/com/newzet/api/subscription/controller/SubscriptionController.java
+++ src/main/java/com/newzet/api/subscription/controller/SubscriptionController.java
@@ -1,7 +1,9 @@
 package com.newzet.api.subscription.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,7 @@ import com.newzet.api.auth.infrastructure.annotation.Login;
 import com.newzet.api.common.response.ResponseCode;
 import com.newzet.api.common.response.SuccessResponse;
 import com.newzet.api.subscription.business.service.SubscriptionQueryRepository;
+import com.newzet.api.subscription.business.service.SubscriptionService;
 import com.newzet.api.subscription.controller.dto.SubscriptionListWithImageResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -22,6 +25,7 @@ import lombok.RequiredArgsConstructor;
 @Tag(name = "구독", description = "구독 관련 API")
 public class SubscriptionController {
 
+	private final SubscriptionService subscriptionService;
 	private final SubscriptionQueryRepository subscriptionQueryRepository;
 
 	@GetMapping
@@ -36,5 +40,17 @@ public class SubscriptionController {
 		);
 		return ResponseEntity.ok(response);
 
+	}
+
+	@DeleteMapping("/{subscriptionId}")
+	@Operation(summary = "뉴스레터 구독 삭제",
+		description = "유저의 뉴스레터 구독을 삭제한다.")
+	public ResponseEntity<SuccessResponse> deleteSubscription(
+		@PathVariable String subscriptionId) {
+		subscriptionService.deleteSubscription(subscriptionId);
+		SuccessResponse<String> response = SuccessResponse.create(
+			ResponseCode.SUCCESS, "뉴스레터 구독 삭제 성공", null);
+
+		return ResponseEntity.ok(response);
 	}
 }
