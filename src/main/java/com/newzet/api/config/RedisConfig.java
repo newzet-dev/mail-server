de51cdfe85bb5e8a4606925b84e0de22db0e4e55
gitjiho <jiho99322@gmail.com>
2025-05-25 17:59:09 +0900

    Feat: 리엑티브 비동기 레디스 사용을 위한 Config 추가

--- src/main/java/com/newzet/api/config/RedisConfig.java
+++ src/main/java/com/newzet/api/config/RedisConfig.java
@@ -5,12 +5,15 @@ import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import io.lettuce.core.ClientOptions;
@@ -37,7 +40,8 @@ public class RedisConfig {
 			.commandTimeout(Duration.ofMillis(500))
 			.clientOptions(ClientOptions.builder()
 				.autoReconnect(true)
-				.socketOptions(SocketOptions.builder().connectTimeout(Duration.ofMillis(1000)).build())
+				.socketOptions(
+					SocketOptions.builder().connectTimeout(Duration.ofMillis(1000)).build())
 				.disconnectedBehavior(ClientOptions.DisconnectedBehavior.REJECT_COMMANDS)
 				.build())
 			.build();
@@ -52,5 +56,22 @@ public class RedisConfig {
 		redisTemplate.setValueSerializer(new StringRedisSerializer());
 		redisTemplate.setConnectionFactory(redisConnectionFactory());
 		return redisTemplate;
+	}
+
+	@Bean
+	public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(
+		ReactiveRedisConnectionFactory connectionFactory) {
+		StringRedisSerializer keySerializer = new StringRedisSerializer();
+		StringRedisSerializer valueSerializer = new StringRedisSerializer();
+
+		RedisSerializationContext<String, String> serializationContext = RedisSerializationContext
+			.<String, String>newSerializationContext()
+			.key(keySerializer)
+			.value(valueSerializer)
+			.hashKey(keySerializer)
+			.hashValue(valueSerializer)
+			.build();
+
+		return new ReactiveRedisTemplate<>(connectionFactory, serializationContext);
 	}
 }
