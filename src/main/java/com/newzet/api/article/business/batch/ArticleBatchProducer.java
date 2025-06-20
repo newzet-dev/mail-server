aa827bb65540be31667a79f3db87ddad0996384d
gitjiho <jiho99322@gmail.com>
2025-06-20 16:11:10 +0900

    Feat: BatchProducer와 BatchConsumer를 상속받아 인터페이스 Article용으로 제작

---
+++ src/main/java/com/newzet/api/article/business/batch/ArticleBatchProducer.java
@@ -0,0 +1,7 @@
+package com.newzet.api.article.business.batch;
+
+import com.newzet.api.article.domain.Article;
+import com.newzet.api.common.batch.BatchProducer;
+
+public interface ArticleBatchProducer extends BatchProducer<Article> {
+}
