4d565afd93ffd72de3193145b56aa7f7d291c26f
gitjiho <jiho99322@gmail.com>
2025-06-20 16:23:17 +0900

    Remove: 구현 완료한 todo 제거

--- src/main/java/com/newzet/api/mail/business/MailServiceFacade.java
+++ src/main/java/com/newzet/api/mail/business/MailServiceFacade.java
@@ -26,7 +26,5 @@ public class MailServiceFacade {
 		subscriptionService.addSubscriptionIfUnsubscribed(userId, fromName, fromDomain,
 			mailingList);
 		articleService.saveArticleBatch(userId, fromName, fromDomain, mailingList, htmlLink, title);
-
-		//TODO: 아티클 저장 후 배치처리 된 애들에 대해서 별도로 event 형식으로 fcm noti 보내는거 추가 (여기 말고)
 	}
 }
