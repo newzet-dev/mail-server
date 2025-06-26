8ee6407686785c20461724804d4242f7978a42c3
gitjiho <jiho99322@gmail.com>
2025-06-27 01:19:35 +0900

    Feat: Fcm 하위 Batch에 추상클래스 상속

--- src/main/java/com/newzet/api/fcm/jpa/batch/FcmRedisBatchProducerImpl.java
+++ src/main/java/com/newzet/api/fcm/jpa/batch/FcmRedisBatchProducerImpl.java
@@ -1,50 +1,40 @@
 package com.newzet.api.fcm.jpa.batch;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Component;
 
+import com.newzet.api.common.batch.AbstractBatchProducer;
 import com.newzet.api.common.objectMapper.OptionalObjectMapper;
 import com.newzet.api.fcm.business.batch.FcmBatchProducer;
 import com.newzet.api.fcm.domain.FcmNotification;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import reactor.core.scheduler.Schedulers;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
-public class FcmRedisBatchProducerImpl implements FcmBatchProducer {
+public class FcmRedisBatchProducerImpl extends AbstractBatchProducer<FcmNotification>
+	implements FcmBatchProducer {
 
 	private static final String FCM_STREAM_KEY = "fcm:stream";
-	private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
-	private final OptionalObjectMapper optionalObjectMapper;
+
+	public FcmRedisBatchProducerImpl(ReactiveRedisTemplate<String, String> reactiveRedisTemplate,
+		OptionalObjectMapper optionalObjectMapper) {
+		super(reactiveRedisTemplate, optionalObjectMapper);
+	}
 
 	@Override
-	public void addToBatch(FcmNotification fcmNotification) {
-		String jsonData = optionalObjectMapper.serialize(fcmNotification);
+	protected String getStreamKey() {
+		return FCM_STREAM_KEY;
+	}
 
-		Map<String, String> fields = new HashMap<>();
-		fields.put("data", jsonData);
+	@Override
+	protected String getItemTypeName() {
+		return "FCM notification";
+	}
 
-		reactiveRedisTemplate.opsForStream()
-			.add(FCM_STREAM_KEY, fields)
-			.subscribeOn(Schedulers.boundedElastic())
-			.doOnSuccess(recordId -> {
-				if (log.isDebugEnabled()) {
-					log.debug(
-						"FCM notification added to batch queue: userId={}, title={}, recordId={}",
-						fcmNotification.getUserId(), fcmNotification.getTitle(), recordId);
-				}
-			})
-			.doOnError(error ->
-				log.error("Failed to add FCM notification to stream: userId={}, title={}, error={}",
-					fcmNotification.getUserId(), fcmNotification.getTitle(), error.getMessage(),
-					error)
-			)
-			.subscribe();
+	@Override
+	protected String getItemIdentifier(FcmNotification fcmNotification) {
+		return String.format("userId=%s, title=%s", fcmNotification.getUserId(),
+			fcmNotification.getTitle());
 	}
 }
