4a1b43bdb56f81d80909364cea0db7bc24544a0f
gitjiho <jiho99322@gmail.com>
2025-06-30 20:53:21 +0900

    Feat: FcmToken 비즈니스 로직에서 FcmSender 관련 로직 분리 및 batch가 아닌 Fcm 메시지 전송 로직 구현

--- src/main/java/com/newzet/api/fcm/business/service/FcmTokenService.java
+++ src/main/java/com/newzet/api/fcm/business/service/FcmTokenService.java
@@ -1,13 +1,10 @@
 package com.newzet.api.fcm.business.service;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
-import com.newzet.api.fcm.business.batch.FcmBatchProducer;
 import com.newzet.api.fcm.business.repository.FcmTokenRepository;
-import com.newzet.api.fcm.domain.FcmNotification;
 import com.newzet.api.fcm.domain.FcmToken;
 
 import lombok.RequiredArgsConstructor;
@@ -19,7 +16,6 @@ import lombok.extern.slf4j.Slf4j;
 public class FcmTokenService {
 
 	private final FcmTokenRepository fcmTokenRepository;
-	private final FcmBatchProducer batchProducer;
 
 	public FcmToken upsertFcmToken(UUID userId, String value) {
 		FcmToken fcmToken = fcmTokenRepository.findIfExistByValue(value)
@@ -31,19 +27,5 @@ public class FcmTokenService {
 	public void deleteFcmToken(UUID userId, String value) {
 		FcmToken fcmToken = fcmTokenRepository.findByUserIdAndValue(userId, value);
 		fcmTokenRepository.deleteFcmToken(fcmToken);
-	}
-
-	public void sendFcmWhenMailReceivedBatch(UUID userId, String fromName, String title) {
-		List<FcmToken> fcmTokens = fcmTokenRepository.findAllByUserId(userId);
-		for (FcmToken fcmToken : fcmTokens) {
-			FcmNotification fcmNotification = FcmNotification.create(userId, fcmToken.value(),
-				fromName, title, null);
-			if (!fcmNotification.isValid()) {
-				log.warn("Invalid FCM notification, skipping: userId={}, token={}",
-					fcmNotification.getUserId(), fcmNotification.getToken());
-				return;
-			}
-			batchProducer.addToBatch(fcmNotification);
-		}
 	}
 }
