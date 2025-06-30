562578a748309948aefc71ea6b808e7b31b5eac2
gitjiho <jiho99322@gmail.com>
2025-06-30 20:53:21 +0900

    Feat: FcmToken 비즈니스 로직에서 FcmSender 관련 로직 분리 및 batch가 아닌 Fcm 메시지 전송 로직 구현

--- src/main/java/com/newzet/api/fcm/orchestrator/FcmTokenOrchestrator.java
+++ src/main/java/com/newzet/api/fcm/orchestrator/FcmTokenOrchestrator.java
@@ -23,8 +23,4 @@ public class FcmTokenOrchestrator {
 	public void deleteFcmToken(UUID userId, String value) {
 		fcmTokenService.deleteFcmToken(userId, value);
 	}
-
-	public void sendFcmWhenMailReceivedBatch(UUID userId, String fromName, String title) {
-		fcmTokenService.sendFcmWhenMailReceivedBatch(userId, fromName, title);
-	}
 }
