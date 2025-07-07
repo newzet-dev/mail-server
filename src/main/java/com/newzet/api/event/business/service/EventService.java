a6ff95c2466aa779d22ed8d78ee73222c54dda2f
anacnoda77 <sm1108shin@naver.com>
2025-07-07 20:06:36 +0900

    Feat: Event Service/DTO 작성

---
+++ src/main/java/com/newzet/api/event/business/service/EventService.java
@@ -0,0 +1,23 @@
+package com.newzet.api.event.business.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.newzet.api.event.business.repository.EventRepository;
+import com.newzet.api.event.domain.Event;
+import com.newzet.api.event.presentation.dto.EventListResponse;
+import com.newzet.api.event.presentation.mapper.EventResponseMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class EventService {
+	private final EventRepository eventRepository;
+
+	EventListResponse getAllEvents() {
+		List<Event> eventList = eventRepository.findEventList();
+		return EventResponseMapper.toListResponse(eventList);
+	}
+}
