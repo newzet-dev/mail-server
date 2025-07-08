3a7500047a8e2be5c84bfbff94fe73199880d487
anacnoda77 <sm1108shin@naver.com>
2025-07-08 15:11:02 +0900

    Feat: 서비스 레이어에 트랜잭션 애노테이션 추가

--- src/main/java/com/newzet/api/event/business/service/EventService.java
+++ src/main/java/com/newzet/api/event/business/service/EventService.java
@@ -3,6 +3,7 @@ package com.newzet.api.event.business.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.newzet.api.event.business.repository.EventRepository;
 import com.newzet.api.event.domain.Event;
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class EventService {
 	private final EventRepository eventRepository;
 
