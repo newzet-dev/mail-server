39d6dd6201ede050c8c5aa5d11c4c054929fef8d
gitjiho <jiho99322@gmail.com>
2025-08-11 15:07:13 +0900

    Fix: jwt 시크릿키 base64 decode로직 추가 및 jwt 파싱 후 name필드 사용 제거

--- src/main/java/com/newzet/api/common/auth/domain/Token.java
+++ src/main/java/com/newzet/api/common/auth/domain/Token.java
@@ -14,15 +14,14 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Token {
 	private final UUID subject;
-	private final String name;
 	private final Date issuedAt;
 	private final Date expiredAt;
 
-	public static Token of(String subject, String name, Date issuedAt, Date expiredAt) {
-		if (subject == null || name == null || issuedAt == null || expiredAt == null) {
+	public static Token of(String subject, Date issuedAt, Date expiredAt) {
+		if (subject == null || issuedAt == null || expiredAt == null) {
 			throw new TokenBadRequestException("토큰 생성에 필요한 값이 누락되었습니다.");
 		}
-		return new Token(UuidConverter.convert(subject), name, issuedAt, expiredAt);
+		return new Token(UuidConverter.convert(subject), issuedAt, expiredAt);
 	}
 
 	public boolean isExpired() {
