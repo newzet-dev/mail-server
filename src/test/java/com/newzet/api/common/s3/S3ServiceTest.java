fb8917243b571f3531f9ac57cb168f70b21fe3ac
JUNG GYUWON <54783290+Gyu-won@users.noreply.github.com>
2025-08-30 13:03:58 +0900

    test: s3Service 메서드 변경에 따른 테스트 코드 수정

--- src/test/java/com/newzet/api/common/s3/S3ServiceTest.java
+++ src/test/java/com/newzet/api/common/s3/S3ServiceTest.java
@@ -21,6 +21,7 @@ import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.newzet.api.common.exception.InternalErrorException;
+import com.newzet.api.config.s3.S3Config;
 
 @ExtendWith(MockitoExtension.class)
 class S3ServiceTest {
@@ -31,6 +32,8 @@ class S3ServiceTest {
 	private S3Service s3Service;
 	@Mock
 	private AmazonS3 amazonS3; // S3Service가 사용하는 SDK v1 클라이언트
+	@Mock
+	private S3Config s3Config;
 
 	@Nested
 	@DisplayName("성공 케이스")
@@ -42,16 +45,16 @@ class S3ServiceTest {
 			String expectedContent = "S3 Object Content";
 			byte[] contentBytes = expectedContent.getBytes(StandardCharsets.UTF_8);
 
-			// S3Object와 그 내용을 Mocking
 			S3Object mockS3Object = new S3Object();
 			S3ObjectInputStream inputStream = new S3ObjectInputStream(
 				new ByteArrayInputStream(contentBytes), null);
 			mockS3Object.setObjectContent(inputStream);
 
+			when(s3Config.getContentBucketName()).thenReturn(BUCKET_NAME);
 			when(amazonS3.getObject(any(GetObjectRequest.class))).thenReturn(mockS3Object);
 
 			// when
-			String actualContent = s3Service.getContentAsString(BUCKET_NAME, OBJECT_KEY);
+			String actualContent = s3Service.getContentAsString(OBJECT_KEY);
 
 			// then
 			assertEquals(expectedContent, actualContent);
@@ -65,12 +68,13 @@ class S3ServiceTest {
 		@DisplayName("S3에 해당 Key의 객체가 없으면 InternalErrorException을 던진다")
 		void getContentAsString_ThrowsNoSuchKeyException() {
 			// given
+			when(s3Config.getContentBucketName()).thenReturn(BUCKET_NAME);
 			when(amazonS3.getObject(any(GetObjectRequest.class)))
 				.thenThrow(new AmazonS3Exception("The specified key does not exist."));
 
 			// when & then
 			assertThrows(InternalErrorException.class, () -> {
-				s3Service.getContentAsString(BUCKET_NAME, OBJECT_KEY);
+				s3Service.getContentAsString(OBJECT_KEY);
 			});
 		}
 
@@ -78,19 +82,18 @@ class S3ServiceTest {
 		@DisplayName("스트림을 읽는 중 IOException이 발생하면 InternalErrorException을 던진다")
 		void getContentAsString_ThrowsIOException() throws IOException {
 			// given
-			// S3Object와 InputStream을 각각 Mocking하여 InputStream의 동작을 제어
 			S3Object mockS3Object = mock(S3Object.class);
 			S3ObjectInputStream mockInputStream = mock(S3ObjectInputStream.class);
 
+			when(s3Config.getContentBucketName()).thenReturn(BUCKET_NAME);
 			when(amazonS3.getObject(any(GetObjectRequest.class))).thenReturn(mockS3Object);
 			when(mockS3Object.getObjectContent()).thenReturn(mockInputStream);
 
-			// 핵심: InputStream에서 readAllBytes() 호출 시 강제로 IOException 발생
 			when(mockInputStream.readAllBytes()).thenThrow(new IOException("스트림 읽기 실패"));
 
 			// when & then
 			assertThrows(InternalErrorException.class, () -> {
-				s3Service.getContentAsString(BUCKET_NAME, OBJECT_KEY);
+				s3Service.getContentAsString(OBJECT_KEY);
 			});
 		}
 	}
